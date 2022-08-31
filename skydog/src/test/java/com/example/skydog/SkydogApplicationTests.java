a7b7f68f58f4cdf0e97e75df85c0e0569cede78b
2019211001000916GanZhou <2592720526@qq.com>
2022-08-31 10:29:37 +0800

    first commit

---
+++ skydog/src/test/java/com/example/skydog/SkydogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skydog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkydogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
