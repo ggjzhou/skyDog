9173792216f9d985d2e9d4a2cc98768d145a99f4
EXPECT <9430613+zhangexpect@user.noreply.gitee.com>
2022-09-06 14:26:40 +0800

    2022-9-6 commit

---
+++ skydog/src/main/java/com/example/skydog/dao/impl/ScaleDaoImpl.java
@@ -0,0 +1,43 @@
+/***********************************************************************
+ * Module:  ScaleDaoImpl.java
+ * Author:  new
+ * Purpose: Defines the Class ScaleDaoImpl
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid 147a84b2-59ad-45bd-9b21-94290a41709b */
+public class ScaleDaoImpl implements ScaleDao {
+   /** @param scale
+    * @pdOid b85b0e88-74b2-4e4c-83c5-f3a79cb9b7a4 */
+   public void add(Scale scale) {
+      // TODO: implement
+   }
+   
+   /** @param scaleId
+    * @pdOid 880f7e71-1c42-451a-8ae2-03e8849ff556 */
+   public void delete(Integer scaleId) {
+      // TODO: implement
+   }
+   
+   /** @param scale
+    * @pdOid 3caa5489-30fc-4d5c-853d-836f155dfd80 */
+   public void updata(Scale scale) {
+      // TODO: implement
+   }
+   
+   /** @param scaleId
+    * @pdOid 93fb1456-afa2-4ddd-b1c0-ef3767bfdb6a */
+   public Scale queryId(Integer scaleId) {
+      // TODO: implement
+      return null;
+   }
+   
+   /** @param scale
+    * @pdOid cdf34293-8e6a-4670-a537-ea236541ec7e */
+   public List<Scale> queryCondition(Scale scale) {
+      // TODO: implement
+      return null;
+   }
+
+}
---
+++ skydog/src/main/java/com/example/skydog/service/impl/ScaleServiceImpl.java
@@ -0,0 +1,46 @@
+/***********************************************************************
+ * Module:  ScaleServiceImpl.java
+ * Author:  new
+ * Purpose: Defines the Class ScaleServiceImpl
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid d0de55f6-d1e4-43be-8c98-20c03e75263d */
+public class ScaleServiceImpl implements ScaleService {
+   /** @param scale
+    * @pdOid 328591b4-d3ea-4b90-9979-69183e507073 */
+   public Boolean add(Scale scale) {
+      // TODO: implement
+      return null;
+   }
+   
+   /** @param scaleId
+    * @pdOid 32a4d703-3ac4-420a-8014-2f2718e2061a */
+   public Boolean delete(Integer scaleId) {
+      // TODO: implement
+      return null;
+   }
+   
+   /** @param scale
+    * @pdOid 753ce19f-326b-4bc1-b31c-ef3734a636fe */
+   public Boolean updata(Scale scale) {
+      // TODO: implement
+      return null;
+   }
+   
+   /** @param scaleId
+    * @pdOid 310eb683-033a-40a0-a0fd-83943f18f565 */
+   public Scale queryId(Integer scaleId) {
+      // TODO: implement
+      return null;
+   }
+   
+   /** @param scale
+    * @pdOid edb6c6d1-37e5-45e1-a2c3-51f65b31f68b */
+   public List<Scale> queryCondition(Scale scale) {
+      // TODO: implement
+      return null;
+   }
+
+}
