6144b6f491dfcf27ce24340f276f61e25167e89f
2019211001000916GanZhou <2592720526@qq.com>
2022-09-13 17:14:08 +0800

    ganzhou 2022-9-13 commit 为商品添加图片实体

--- skydog/src/main/java/com/example/skydog/module/entity/Product.java
+++ skydog/src/main/java/com/example/skydog/module/entity/Product.java
@@ -25,4 +25,6 @@ public class Product implements Serializable {
     private String productStatus;//商品状态
     private Long productSale;//商品销量
     private Double productGrade;//商品评分
+    private Integer imgId;
+    private Img img;
 }
